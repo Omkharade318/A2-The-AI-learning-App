5da2192991de9d109a4c7e0e688f4ead6871d37c
Om kharade <kharadeoms@gmail.com>
2024-11-13 10:58:03 +0530

    fixed sign up page

--- app/src/main/java/com/example/a2lytics/drawer/Edit_Profile.kt
+++ app/src/main/java/com/example/a2lytics/drawer/Edit_Profile.kt
@@ -3,6 +3,8 @@ package com.example.a2lytics.drawer
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.a2lytics.R
 
 class Edit_Profile : AppCompatActivity() {
@@ -10,9 +12,11 @@ class Edit_Profile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_edit_profile)
-
-        
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
     }
 }
