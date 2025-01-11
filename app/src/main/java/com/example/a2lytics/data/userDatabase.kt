a97a1703cc15b5380475399dae6a3226998651d3
Om kharade <kharadeoms@gmail.com>
2025-01-11 18:17:51 +0530

    completed database schema

---
+++ app/src/main/java/com/example/a2lytics/data/userDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.a2lytics.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import android.content.Context
+import androidx.room.Room
+
+@Database(entities = [User::class], version = 1, exportSchema = false)
+abstract class UserDatabase: RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: UserDatabase? = null
+
+        fun getDatabase(context: Context): UserDatabase{
+            val tempInstance = INSTANCE
+            if (tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    UserDatabase::class.java,
+                    "user_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
